790fe428cb7e7bf78861aa70751a3c9fed66a395
ognentrajkovski <ognen.trajkovski50@gmail.com>
2026-04-16 11:59:25 +0200

    Add REST controller, CORS configuration and application properties

--- backend/src/main/java/com/codeit/earthquake/config/AppConfig.java
+++ backend/src/main/java/com/codeit/earthquake/config/AppConfig.java
@@ -1,14 +1,32 @@
 package com.codeit.earthquake.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
 
+    @Value("${spring.web.cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(allowedOrigins)
+                        .allowedMethods("GET", "POST", "DELETE", "OPTIONS");
+            }
+        };
     }
 }
