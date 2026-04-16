ff684cb2280382fd4b9d70e521b62a0b76a2bda7
ognentrajkovski <ognen.trajkovski50@gmail.com>
2026-04-16 11:01:47 +0200

    Add custom exceptions and global exception handler

---
+++ backend/src/main/java/com/codeit/earthquake/exception/EarthquakeNotFoundException.java
@@ -0,0 +1,8 @@
+package com.codeit.earthquake.exception;
+
+public class EarthquakeNotFoundException extends RuntimeException {
+
+    public EarthquakeNotFoundException(Long id) {
+        super("Earthquake not found with id: " + id);
+    }
+}
