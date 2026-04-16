cfd21506c3cc15e986ac11702ad015491584ecbe
ognentrajkovski <ognen.trajkovski50@gmail.com>
2026-04-16 10:59:37 +0200

    Add Earthquake entity, JPA repository, and GeoJSON DTOs

---
+++ backend/src/main/java/com/codeit/earthquake/dto/GeoJsonResponse.java
@@ -0,0 +1,43 @@
+package com.codeit.earthquake.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GeoJsonResponse {
+
+    private List<Feature> features;
+
+    @Data
+    @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Feature {
+        private String id;
+        private Properties properties;
+        private Geometry geometry;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Properties {
+        private Double mag;
+        private String magType;
+        private String place;
+        private String title;
+        private Long time;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Geometry {
+        /** coordinates: [longitude, latitude, depth] */
+        private List<Double> coordinates;
+    }
+}
